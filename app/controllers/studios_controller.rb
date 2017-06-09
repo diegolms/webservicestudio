b695f7ba153ee1f1963e912e07227530207115c6
Diego Lopes <diego.silv@control.eng.br>
2017-06-09 17:03:22 -0300

    criacao de studio

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -1,5 +1,7 @@
 class StudiosController < ApplicationController
 
+  skip_before_action :verify_authenticity_token
+
   #lista todos os studios cadastrados na base
   def listar_studios
     render :json => Studio.all.to_json
@@ -7,11 +9,27 @@ class StudiosController < ApplicationController
 
   #cadastra um novo studio
   def criar_studio
+    begin
+      result = Hash.new
       studio = Studio.new
-      studio.nome = "teste"
-      studio.latitude = -3.084506666666667
-      studio.longitude = "-60.000701666666664"
-      studio.save!
-      render :json => studio.to_json
+       studio.nome = "teste"
+      # studio.latitude = "-3.084506666666667"
+      # studio.longitude = "-60.000701666666664"
+      if studio.save
+        result[:status] = 200
+        result[:message] = "Studio criado com sucesso"
+        result[:studio] = studio
+      else
+        result[:status] = 500
+        result[:message] = studio.errors
+      end
+    rescue Exception => ex
+
+    end
+
+
+    render :json => result.to_json
+
+      # p "params #{params}"
   end
 end
--- app/models/studio.rb
+++ app/models/studio.rb
@@ -1,3 +1,5 @@
 class Studio < ActiveRecord::Base
   belongs_to :arquivo
+
+  validates_presence_of :nome, :message => "Nome do studio inválido"
 end
